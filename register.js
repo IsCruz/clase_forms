0108cd8bef3bf810d7d9ad9845b36b1831ddf296
Israel Cruz Ibarra <israel.ibarra@ibm.com>
2019-10-11 20:30:22 -0500

    Add POST and updateUI function

--- register.js
+++ register.js
@@ -1,8 +1,27 @@
 const registerButton = document.getElementById('register');
 
-registerButton.addEventListener('click', printValues);
+registerButton.addEventListener('click', postUser);
 
-window.addEventListener('load', getAsyncUsers);
+window.addEventListener('load', getUsers);
+// add users container element
+const usersContainer = document.getElementById('myUsers');
+// updateUI function
+function updateUI(users) {
+    // get an array of GET users request.
+    users.forEach(user => {
+    // for each element, create a new div with data
+     const newElement = `
+     <div class="card">
+     <div class="card-body">
+        <h5 class="card-title">${user.username}</h5>
+        <p class="card-text">Created by: ${user.name}</p>
+      </div>
+     </div>
+     <br>`;
+     // add new DIV to our usersContainer
+     usersContainer.insertAdjacentHTML("beforeend", newElement);
+    });
+}
 
 function getUsers(){
     const headers = new Headers({'Content-Type': 'application/json'});
@@ -11,7 +30,7 @@ function getUsers(){
     headers: headers
     }).then(response => {
         return response.json();
-    }).then(res => console.log(res));
+    }).then(res => updateUI(res));
 }
 
 async function getAsyncUsers(){
@@ -22,6 +41,45 @@ async function getAsyncUsers(){
     const parseJSON = await APIcall.json();
     const data = await parseJSON;
     console.log(data);
+}
+
+function postUser(event) {
+    // avoid default behaviour
+    event.preventDefault();
+    // get data values;
+    const data = {
+        name: document.getElementById('name').value,
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
+    };
+    // update UI
+    updateUI([data]);
+    // define headers
+    const headers = new Headers({'Content-Type': 'application/json'});
+    // pass object to JSON
+    const transformData = JSON.stringify(data);
+    /* 
+     JSON example => {
+         "name" : "myname",
+         "username": "myusername",
+         "password": "mypass"
+     }
+    */
+    // make POST Call, create user in db.json
+    return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: headers,
+    body: transformData
+    }).then(response => {
+        return response.json();
+    }).then(res => {
+        console.log(res);
+        // erase past info
+        document.getElementById('name').value = '';
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+    });
+
 }
 
 function printValues(event){
